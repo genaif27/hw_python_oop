2e512a230ff3dbb65720d7656e4808f35b6099f3
Gennady Lee <gennadylee@gmail.com>
2022-09-26 21:04:37 +0300

    damn again 29 sep

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, asdict
-from typing import Dict, Type
+from typing import ClassVar, Type
 
 
 @dataclass
@@ -11,11 +11,11 @@ class InfoMessage:
     speed: float
     calories: float
 
-    MESSAGE = ('Тип тренировки: {training_type}; '
-               'Длительность: {duration:.3f} ч.; '
-               'Дистанция: {distance:.3f} км; '
-               'Ср. скорость: {speed:.3f} км/ч; '
-               'Потрачено ккал: {calories:.3f}.')
+    MESSAGE: ClassVar[str] = ('Тип тренировки: {training_type}; '
+                              'Длительность: {duration:.3f} ч.; '
+                              'Дистанция: {distance:.3f} км; '
+                              'Ср. скорость: {speed:.3f} км/ч; '
+                              'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         return self.MESSAGE.format(**asdict(self))
@@ -23,9 +23,9 @@ class InfoMessage:
 
 class Training:
     """Базовый класс тренировки."""
-    training_type: str
     LEN_STEP: float = 0.65
-    M_IN_KM: int = 1000
+    M_IN_KM: float = 1000
+    MIN_IN_H: float = 60
 
     def __init__(self,
                  action: int,
@@ -54,9 +54,8 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    COEFF_CALORIE_1: int = 18
-    COEFF_CALORIE_2: int = 20
-    MIN_IN_H: int = 60
+    COEFF_CALORIE_1: float = 18
+    COEFF_CALORIE_2: float = 20
 
     def get_spent_calories(self) -> float:
         return ((self.COEFF_CALORIE_1 * self.get_mean_speed()
@@ -69,7 +68,6 @@ class SportsWalking(Training):
     COEFF_WALKING_1: float = 0.035
     COEFF_WALKING_2: float = 0.029
     COEFF_WALKING_3: float = 2
-    MIN_IN_H: int = 60
 
     def __init__(self,
                  action: int,
@@ -113,13 +111,13 @@ class Swimming(Training):
 
 
 def read_package(
-    workout_type: Type[str],
+    workout_type: str,
     data: list,
 ) -> Training:
 
     """Прочитать данные полученные от датчиков."""
 
-    workout_type_classes: Dict[str, str] = {
+    workout_type_classes: Type[Training] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking,
